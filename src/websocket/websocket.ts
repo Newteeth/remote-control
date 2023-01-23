cd6e7573042ff8502c86a6825b713a770e3c38d3
Anatoliy <antononanatoliy82@gmail.com>
2023-01-24 03:15:29 +0500

    refactor: chenge option in createWebSocketStream from utf-8 to the decodeStrings: false. change event from 'massege' to the 'data'

--- src/websocket/websocket.ts
+++ src/websocket/websocket.ts
@@ -7,10 +7,10 @@ export const wss = new WSWebSocketServer({ port: PORT_WEBSOCKET });
 wss.on('connection', async ws => {
 
     process.stdout.write(`Start connect websocket on ${PORT_WEBSOCKET} port\n`);
-    createWebSocketStream(ws, ({encoding: 'utf-8'}));
 
-    ws.on('message', async data => {
-        
+    let wsStreem = createWebSocketStream(ws, ({ decodeStrings: false }));
+
+    wsStreem.on('data', async data => {
         const resWeb = JSON.stringify(`${data}`).replace(/"/g, '').split(' ');
         let str = JSON.stringify(await hendlerStart(resWeb));
         str = str.replace(/["]/g, '');
